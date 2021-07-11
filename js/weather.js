2a841210f46b2b83e5c7c5976f61bde5454fb706
jinseonjeon <jjseon0501@gmail.com>
2021-07-12 00:01:24 +0900

    Add weather API

---
+++ js/weather.js
@@ -0,0 +1,20 @@
+const API_KEY = "519d899d6f2836ef122bde74c33d9513";
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const log = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+    console.log(url);
+    fetch(url).then(response => response.json()).then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGepError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGepError)
